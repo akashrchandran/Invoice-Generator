b30a05ecc1ef710d3dfd99da6b01a052d454307d
Akash R Chandran <chandranrakash@gmail.com>
2023-02-18 21:17:13 +0530

    updated

--- index.py
+++ index.py
@@ -1,8 +1,17 @@
 import base64
+import os
 import random
 from datetime import date, datetime
+from mongo import add_invoice
+import requests
+from werkzeug.datastructures import MultiDict
 
 from flask import Flask, render_template, request
+
+import dotenv as dotenv
+
+dotenv.load_dotenv()
+
 
 app = Flask(__name__)
 
@@ -15,7 +24,7 @@ def index():
 def invoice():
     data=request.form
     image = request.files['sp_logo'].read()
-    image_base64 = base64.b64encode(image).decode('utf-8')
+    img_url = upload_image(image)
     invoice_date = datetime.strptime(data['invoice_date'], '%Y-%m-%d').date().strftime("%B %d, %Y")
     invoice_due_date = datetime.strptime(data['invoice_due_date'], '%Y-%m-%d').date().strftime("%B %d, %Y")
     item_name = data.getlist('item_name[]')
@@ -29,7 +38,31 @@ def invoice():
         items.append({'item_name': item_name[x], 'item_qty': item_qty[x], 'item_unitprice': item_unitprice[x], 'amount': amount})
     tax = int(data['tax_percentage']) * sub_total / 100
     total = sub_total + tax
-    return render_template('invoice.html', image=image_base64, data=data, invoice_date=invoice_date, invoice_due_date=invoice_due_date, items=items, sub_total=sub_total, tax=tax, total=total)
+    form = MultiDict(request.form)
+    form['image'] = img_url
+    form['invoice_date'] = invoice_date
+    form['invoice_due_date'] = invoice_due_date
+    form['items'] = items
+    form['sub_total'] = sub_total
+    form['tax'] = tax
+    form['total'] = total
+    add_invoice(form.to_dict())
+    return render_template('invoice.html', image=img_url, data=data, invoice_date=invoice_date, invoice_due_date=invoice_due_date, items=items, sub_total=sub_total, tax=tax, total=total)
+
+@app.route('/share', methods=['GET'])
+def share(encoded_id):
+    return render_template('invoice.html', image=img_url, data=data, invoice_date=invoice_date, invoice_due_date=invoice_due_date, items=items, sub_total=sub_total, tax=tax, total=total)
+
+def upload_image(image_data):
+    url = "https://api.imgbb.com/1/upload"
+    payload = {
+        "key": os.environ.get('ImgBB_API_KEY'),
+    }
+    files = {
+        "image": image_data
+    }
+    response = requests.post(url, data=payload, files=files)
+    return response.json()['data']['url']
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
--- mongo.py
+++ mongo.py
@@ -1,9 +1,14 @@
 import pymongo
 import os
 
+import dotenv as dotenv
+
+dotenv.load_dotenv()
+
 client = pymongo.MongoClient(os.environ.get('MONGO_URI'))
 db = client.invoice_generator
 invoice = db.invoice
 
 def add_invoice(data):
+    print(data)
     invoice.insert_one(data)
